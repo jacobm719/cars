92385874f9c660499bc192fb957b75fadd1eb840
Jacob Myung <jacobmyung719@gmail.com>
2023-01-09 22:15:17 -0500

    First commit

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -44,25 +44,57 @@ class Cars extends React.Component {
 
     // const newState = JSON.parse(JSON.stringify(this.state));
 
-    const newState = {
-      ...this.state,
-      carsData: {
-        ...this.state.carsData,
-        cars: {
-          ...this.state.carsData.cars,
-          sedan: this.state.carsData.sedan.map((item, index) => {
-            if (index === 0) {
-              return { ...item, number: item.number - 1 };
-            } else {
-              return item;
-            }
-          }),
-        },
-      },
-    };
-    this.setState(newState);
-  };
+    //   const newState = {
+    //     ...this.state,
+    //     carsData: {
+    //       ...this.state.carsData,
+    //       cars: {
+    //         ...this.state.carsData.cars,
+    //         sedan: this.state.carsData.sedan.map((item, index) => {
+    //           if (index === 0) {
+    //             return { ...item, number: item.number - 1 };
+    //           } else {
+    //             return item;
+    //           }
+    //         }),
+    //       },
+    //     },
+    //   };
+    //   this.setState(newState);
+    // };
 
+    if (this.state.carsData.cars[category][index].number === 0) return;
+    this.setState((prev) => {
+      return {
+        ...prev,
+        carsData: {
+          ...prev.carsData,
+          cars: {
+            ...prev.carsData.cars,
+            [category]: [
+              ...prev.carsData.cars[category].slice(0, index),
+              {
+                ...prev.carsData.cars[category][index],
+                number: prev.carsData.cars[category][index].number - 1,
+              },
+              ...prev.carsData.cars[category].slice(index + 1),
+            ],
+            // ...prev.carsData.cars[category].map((item, innerIndex) => {
+            //   if (index === innerIndex) {
+            //     return {
+            //       ...prev.carsData.cars[category][index],
+            //       number: prev.carsData.cars[category][index].number - 1,
+            //     };
+            //   } else {
+            //     return item;
+            //   }
+            // }),
+          },
+          totalNumber: prev.carsData.totalNumber - 1,
+        },
+      };
+    });
+  };
   render() {
     return (
       <div className="cars">
